355285bb55b638d915697a48d047eaabf04488c0
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-11-19 10:56:23 +0530

    Update ProxyOwnershipERC721.sol

--- ProxyOwnershipERC721.sol
+++ ProxyOwnershipERC721.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "../interfaces/IProxyOwnership.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "./IProxyOwnership.sol";
+import "./ERC721Enumerable.sol";
 
 /// Provides a transitive closure over ownership relations for NFTs containing
 /// other assets
