6ef226f023918ad4eb7536e97fe482a7c1438952
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-11-19 10:53:56 +0530

    Update Executor.sol

--- Executor.sol
+++ Executor.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "./roles/RoleAware.sol";
+import "./RoleAware.sol";
 
 /// Execute complex transactions on contracts within
 /// within roles system with ownership privileges
